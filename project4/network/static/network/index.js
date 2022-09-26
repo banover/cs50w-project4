e6a882e050a29087bcf77d8f0becefe8a0bb5b7b
banover <qkrwotn5713@naver.com>
2022-09-26 23:24:45 +0900

    New Post ~ing

---
+++ project4/network/static/network/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('#new_post_form').onsubmit = upload_db;
+})
+
+function upload_db() {
+    // Setting post data into variables 
+    
+    // upload variable into db using fetch  
+
+    // return false (이거 위에 display none과 block 활용하기)
+}
